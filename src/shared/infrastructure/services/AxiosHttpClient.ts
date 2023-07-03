e0197c1dfd644763d35353f67cc8f5fa0c9a039d
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2023-07-03 14:10:25 +0300

    feat(shared/infrastructure): add axios based http client

---
+++ src/shared/infrastructure/services/AxiosHttpClient.ts
@@ -0,0 +1,37 @@
+/* eslint-disable max-len */
+
+import axios, { AxiosInstance } from 'axios';
+
+import { HttpClient, RequestConfig } from 'src/shared/infrastructure/interfaces';
+
+export class AxiosHttpClient implements HttpClient {
+  private http: AxiosInstance;
+
+  constructor() {
+    this.http = axios.create();
+  }
+
+  async get<T>(url: string, config?: RequestConfig | undefined): Promise<T> {
+    const response = await this.http.get(url, config);
+
+    return response.data;
+  }
+
+  async delete<T>(url: string, config?: RequestConfig | undefined): Promise<T> {
+    const response = await this.http.delete(url, config);
+
+    return response.data;
+  }
+
+  async post<T, D>(url: string, data?: D | undefined, config?: RequestConfig | undefined): Promise<T> {
+    const response = await this.http.post(url, data, config);
+
+    return response.data;
+  }
+
+  async put<T, D>(url: string, data?: D | undefined, config?: RequestConfig | undefined): Promise<T> {
+    const response = await this.http.put(url, data, config);
+
+    return response.data;
+  }
+}
