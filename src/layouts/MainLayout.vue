b0425b758dede2a6a571a009ad6898834d88b1d9
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2023-07-13 23:27:29 +0300

    feat(layouts/main): remove drawer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,35 +1,14 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-
-const leftDrawerOpen = ref(false);
-
-const toggleLeftDrawer = () => {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
-};
-</script>
-
 <template>
   <q-layout view="lHh lpr lFf">
 
     <q-header class="bg-white text-black" bordered>
       <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
-
+        <q-icon name="bookmarks" color="primary" size="md" />
         <q-toolbar-title>
           Booky
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
-
-    <q-drawer
-      class="bg-grey-1 text-black"
-      show-if-above
-      v-model="leftDrawerOpen"
-      side="left"
-      bordered
-    >
-      <!-- drawer content -->
-    </q-drawer>
 
     <q-page-container>
       <router-view />
